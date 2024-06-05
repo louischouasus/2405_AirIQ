88d8b39f83d234322242d703bca62a800a6486b6
louischouasus <127476081+louischouasus@users.noreply.github.com>
2024-06-05 13:18:07 +0800

    0605

--- draw.py
+++ draw.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.pylab import mpl
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import sys
+import numpy as np
+import random
+import time
 
 
 class Graph(tk.Frame):
@@ -11,6 +14,9 @@ class Graph(tk.Frame):
         self.master = master
         self.fig, self.ax = plt.subplots(2, 2, figsize=(8, 6))
         self.ax[0][0].plot([1, 1.4, 3.5, 4], [1, 4, 9, 16])
+        x = np.array(["A", "B", "C", "D"])
+        y = np.array([12, 22, 6, 18])
+        self.ax[1][1].bar(x, y, color=["#4CAF50", "red", "hotpink", "#556B2F"])
         self.canvas = FigureCanvasTkAgg(self.fig, root)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
@@ -19,15 +25,23 @@ class Graph(tk.Frame):
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         master.protocol("WM_DELETE_WINDOW", self.closeWindow)
         self.update()
+        self.data = [10]
 
     def closeWindow(self):
         sys.exit()
 
-    def update_data(self, target, data):
-        self.ax[target[0]][target[1]].clear()
-        self.ax[target[0]][target[1]].plot([1, 2, 3.5, 4], [1, 4, 9, 16])
+    def update_data(self):
+        self.data.append(self.data[-1] + random.randint(-3, 3))
+        self.ax[0][1].clear()
+        self.ax[0][1].plot([i for i in range(len(self.data))], self.data)
 
 
 root = tk.Tk()
 app = Graph(master=root)
+while True:
+    app.update_data()
+    app.update()
+    app.canvas.draw()
+    time.sleep(1)
+
 app.mainloop()
