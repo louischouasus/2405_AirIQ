73e95325b7b2aa304779f34e1bb8cc38c8af40bd
louischouasus <stupidegg8576@gmail.com>
2024-06-28 18:26:21 +0800

    SDS

--- test3.py
+++ test3.py
@@ -1,9 +1,5 @@
-import time
-
-while True:
-    try:
-        print("Hello")
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print("asdasd")
-        break
+https://www.cnblogs.com/zyg123/p/10385456.html
+https://stackoverflow.com/questions/65385298/push-buttons-in-tkinter-and-matplotlib
+https://stackoverflow.com/questions/6920302/how-to-pass-arguments-to-a-button-command-in-tkinter
+https://github.com/PaddlePaddle/FastDeploy
+https://www.paddlepaddle.org.cn/documentation/docs/zh/guides/beginner/model_save_load_cn.html
